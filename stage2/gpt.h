720a67741639a89236648ba2f5867947864d4482
Richard Yao <ryao@cs.stonybrook.edu>
2012-10-29 06:21:34 -0400

    Make gpt.h use types.h
    Signed-off-by: Richard Yao <ryao@cs.stonybrook.edu>

--- stage2/gpt.h
+++ stage2/gpt.h
@@ -20,14 +20,7 @@
 #ifndef _GPT_H
 #define _GPT_H
 
-typedef signed char grub_int8_t;
-typedef signed short grub_int16_t;
-typedef signed int grub_int32_t;
-typedef signed long long int grub_int64_t;
-typedef unsigned char grub_uint8_t;
-typedef unsigned short grub_uint16_t;
-typedef unsigned int grub_uint32_t;
-typedef unsigned long long int grub_uint64_t;
+#include "types.h"
 
 struct grub_gpt_header
 {
